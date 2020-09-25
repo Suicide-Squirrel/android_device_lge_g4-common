bdff01c171ab7e155b31303bc98175085cbe8b64
pavlaras <theodoroupaulos@gmail.com>
2020-09-25 08:21:44 +0300

    g4-common: BoardConfigCommon: Cleanup duplicated flags
    Change-Id: I3a8b337e2a6f343578d3e42f23c6aba37af60609

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -167,17 +167,15 @@ EXTENDED_FONT_FOOTPRINT := true
 TARGET_PROVIDES_LIBLIGHT := true
 
 # Offmode Charging
+WITH_CM_CHARGER := false
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
-#BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_ENABLE_SUSPEND := false
 
 COMMON_GLOBAL_CFLAGS += \
     -DBOARD_CHARGING_CMDLINE_NAME='"androidboot.mode"' \
     -DBOARD_CHARGING_CMDLINE_VALUE='"chargerlogo"'
 
 # Power
-WITH_CM_CHARGER := false
-BOARD_CHARGER_DISABLE_INIT_BLANK := true
-BOARD_CHARGER_ENABLE_SUSPEND := false
 TARGET_POWERHAL_VARIANT := qcom
 BACKLIGHT_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/lge_touch/tap2wake"
